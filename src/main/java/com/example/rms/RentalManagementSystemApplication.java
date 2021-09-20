d5aae99222c07e5915563bd17062ed57998bb3f5
Ashisoma <okalionesmus@gmail.com>
2021-09-20 09:51:20 +0300

    final refactoring

--- src/main/java/com/example/rms/RentalManagementSystemApplication.java
+++ src/main/java/com/example/rms/RentalManagementSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RmsApplication {
+public class RentalManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RmsApplication.class, args);
+		SpringApplication.run(RentalManagementSystemApplication.class, args);
 	}
 
 }
