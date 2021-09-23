67b5aeb4868999b4fe74ad95aab8eee51ddf7745
Ashisoma <okalionesmus@gmail.com>
2021-09-23 13:10:42 +0300

    admin end points

--- src/main/java/com/example/rms/config/AdminConfig.java
+++ src/main/java/com/example/rms/config/AdminConfig.java
@@ -10,9 +10,12 @@ import java.time.LocalDate;
 
 @Configuration
 public class AdminConfig {
+
     @Bean
     CommandLineRunner runner(AdminRepository adminRepository){
         return args -> {
+            // this is just soem data i wanted to use on the db rather than have an empty table. The db is empty and
+            // this is like cache data
             Admin landlord1 = new Admin("Peter","Muturi", 3242199,"yeye@gmail.com", "1234", LocalDate.of(1998, 4, 24)
                     ,true,"male");
             adminRepository.save(landlord1);
